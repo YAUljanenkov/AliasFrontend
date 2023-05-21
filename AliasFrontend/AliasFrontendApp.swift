52ce2bfdc05ad0c685d9fb9040690d47fe1e8785
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-05-21 17:39:39 +0300

    Initial Commit

---
+++ AliasFrontend/AliasFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasFrontendApp.swift
+//  AliasFrontend
+//
+//  Created by Ярослав Ульяненков on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AliasFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
