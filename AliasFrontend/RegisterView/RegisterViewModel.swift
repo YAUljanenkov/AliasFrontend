b5b7c066719270c4d159438b32fd3c557bd74366
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-05-23 16:44:43 +0300

    add login after registration

--- AliasFrontend/RegisterView/RegisterViewModel.swift
+++ AliasFrontend/RegisterView/RegisterViewModel.swift
@@ -25,8 +25,26 @@ class RegisterViewModel: ObservableObject {
         dataManager.register(name: name, email: login, password: password) {[weak self] result in
             switch result {
             case .success(let value):
-                // TODO: add action.
                 print(value)
+                guard let login = self?.login, let password = self?.password else {
+                    self?.showAlert.toggle()
+                    self?.errorMessage = "Неизвестная ошибка"
+                    return
+                }
+                
+                // Registration does not log in, we need to do it afterwards to get bearer token.
+                self?.dataManager.login(email: login, password: password) {result in
+                    switch result {
+                    case .success(let value):
+                        // TODO: add action.
+                        print(value)
+                    case .error(let error):
+                        // TODO: add proper check.
+                        print(error)
+                        self?.showAlert.toggle()
+                        self?.errorMessage = error.localizedDescription
+                    }
+                }
             case .error(let error):
                 // TODO: add proper check.
                 print(error)
